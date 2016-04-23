822fb7b6e16660522e994661fd4d33b92681db3c
Kateryna Khotkevych <khotkevych@hotmail.com>
2016-04-24 00:54:38 +0300

    + qestion

--- core/matchers/templateMatcher.js
+++ core/matchers/templateMatcher.js
@@ -4,21 +4,34 @@ var templates = [
   {
     keywordGroups: [["Sasha", "Alex", "Boss"], ["Photo","Image", "Avatar"]],
     present: function(presenter){
-      presenter.showImage("https://scontent-waw1-1.xx.fbcdn.net/hphotos-xtp1/t31.0-8/10931657_860658840660717_3580588527443262148_o.jpg")
+      return presenter.showImage("https://scontent-waw1-1.xx.fbcdn.net/hphotos-xtp1/t31.0-8/10931657_860658840660717_3580588527443262148_o.jpg")
     }
   },
   {
     keywordGroups: [["toilet", "pipi", "bahtrom", "potty", "poop"], ["astronauts","astronaut", "cosmonaut", "cosmonauts"]],
     present: function(presenter){
-        presenter.showAutor(autors.iss).then(function(){
-          presenter.showText(
+       return presenter.showAutor(autors.iss).then(function(){
+          return presenter.showText(
             "- Oh, those human limitations... Tim Peake answers one of the most frequently asked question about space exploration. Speaking from the International Space Station, the British astronaut explains how he and his colleagues manage the bodily function using a machine with a suction tube:"
           );
         }).then(function(){
-          presenter.showText("https://www.youtube.com/watch?v=d4FqnPPJ-Vc");
+          return presenter.showText("https://www.youtube.com/watch?v=d4FqnPPJ-Vc");
         })
     }
   },
+  {
+    keywordGroups: [["eclipse"], ["solar","sun"], ["will", "next", "new"]],
+    present: function(presenter){
+        return presenter.showAutor(autors.nasa).then(function(){
+          return presenter.showText(
+            "The next solar eclipse will be of 2016 September 1.\nDon’t miss it!"
+          );
+        }).then(function(){
+          return presenter.showImage("https://timedotcom.files.wordpress.com/2016/03/solar-eclipse-01.jpg?quality=75&strip=color&w=792");
+        })
+    }
+  },
+]
 ]
 
 exports.match = function(str){
