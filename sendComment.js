123ba5b5cb037208e4748cb25e42e9c7a35440a9
Pavlo Razumovskiy <onum.mail@gmail.com>
2016-04-24 01:32:26 +0300

    [feature] send commet

---
+++ sendComment.js
@@ -0,0 +1,53 @@
+var request = require("request");
+var Promise = require("bluebird");
+
+
+function sendText(sender, text) {
+	messageData = {
+		text:text
+	}
+	return sendToUser(sender, messageData)
+}
+
+function sendImage(sender, url) {
+	messageData = {
+    "attachment":{
+      "type":"image",
+      "payload":{
+        "url":url
+      }
+    }
+	}
+	return sendToUser(sender,messageData)
+}
+
+function sendToUser(sender, messageData){
+	return new Promise(function (resolve, reject) {
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: {access_token:token},
+			method: 'POST',
+			json: {
+				recipient: {id:sender},
+				message: messageData,
+			}
+		}, function(error, response, body) {
+			if (error) {
+				console.log('Error sending messages: ', error)
+				return reject();
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error)
+				return reject();
+			}
+			return resolve();
+		})
+	});
+};
+
+sendImage("http://assets.mshanken.com/wso/Articles/2015/UN_Comet102915_1600.jpg").then(function(){
+  return sendText("Comet news!");
+}).then(function(){
+  return sendText("Some amazing news here!");
+}).then(function(){
+  return sendImage("http://24space.ru/uploads/posts/2015-04/1429830878_spacex-reusable-rocket-test.jpg")
+});
